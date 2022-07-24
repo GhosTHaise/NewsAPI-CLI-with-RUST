999648128cb901e89a1368e792aed8d3eeb954ec
GhosTHaise <Ghostrex2@gmail.com>
2022-07-24 17:54:20 +0300

    refractoring the cli

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,6 @@
-use std::error::Error;
-use serde::Deserialize;
 use colour::{dark_green,yellow};
-
-
-#[derive(Deserialize,Debug)]
-struct Articles {
-    articles : Vec<Article>
-}
-
-#[derive(Deserialize,Debug)]
-struct Article{
-    title : String,
-    url: String
-}
-
-fn get_articles(url : &str) -> Result<Articles,Box<dyn Error>>{
-    let response= ureq::get(url).call()?.into_string()?;
-
-    let articles: Articles = serde_json::from_str(&response)?;
-
-    Ok(articles)
-}
+use std::error::Error;
+use newsApi::{get_articles,Articles};
 
 fn render_articles(articles:&Articles)-> (){
     for i in &articles.articles {
